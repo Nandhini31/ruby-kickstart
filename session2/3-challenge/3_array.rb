2adb86acee4a719649bcaa3f87f063fb0eaa054a
nandhini <nandhini31@gmail.com>
2017-06-17 10:22:31 +0100

    added 3_array.rb

--- session2/3-challenge/3_array.rb
+++ session2/3-challenge/3_array.rb
@@ -7,6 +7,12 @@
 
 class String
   def every_other_char
+    my_string = ''
+    each_char.with_index do |char,index|
+      my_string << char if index.even?
+    end
+    return my_string
+
   end
-  
+
 end
