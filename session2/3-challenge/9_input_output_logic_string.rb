e903f90b5c9f2863232fd744af7e5786e5b6d958
nandhini <nandhini31@gmail.com>
2017-06-17 17:06:46 +0100

    9_input_output_logic_string.rb

--- session2/3-challenge/9_input_output_logic_string.rb
+++ session2/3-challenge/9_input_output_logic_string.rb
@@ -25,8 +25,17 @@
 
 def deaf_grandma
 
+  while line = gets
+    line.chomp!
+    if line == 'BYE'
+      break
+    elsif line == line.upcase && line !=""
+      puts  "NO, NOT SINCE 1938!"
+    else
+      puts   "HUH?! SPEAK UP, SONNY!"
+    end
+  end
 end
-
 
 
 
