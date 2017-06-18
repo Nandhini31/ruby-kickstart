ebc1b25fb6c84094d8286a3e90f8512ab9e4349e
nandhini <nandhini31@gmail.com>
2017-06-18 17:54:56 +0100

    11_classes.rb

--- session2/3-challenge/11_classes.rb
+++ session2/3-challenge/11_classes.rb
@@ -20,3 +20,52 @@
 # if the parameter is greater than 99, set the number of beer bottles to 99
 # Then make a public method called print_song that outputs all stanzas from the number of bottles of beer down to zero.
 # Add any additional methods you find helpful.
+class BeerSong
+
+
+  attr_accessor :number_of_bottles
+
+
+  def initialize(number_of_bottles)
+     number_of_bottles = 0 if number_of_bottles < 0
+     number_of_bottles = 99 if number_of_bottles > 99
+    self.number_of_bottles = number_of_bottles
+  end
+
+  def print_number_of_times(number_of_bottles)
+      number_of_bottles.downto 1 do |i|
+        print_song i
+      end
+  end
+
+
+  def print_song(n)
+    if n.zero?
+      String.new
+    else
+      puts  " #{translate n} #{bottles} of beer on the wall,",
+            " #{translate n } #{bottles}  of beer,",
+            " one down, pass it around,",
+            "#{translate n -1} #{bottles} of beer on the wall."
+    end
+  end
+
+  def bottles(n)
+    if n == 1
+      number_of_bottles = 'bottle'
+    else
+      number_of_bottles = 'bottles'
+    end
+  end
+
+
+  def translate(n)
+    if 0 <= n && n <= 19
+      %w(zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen)[n]
+    elsif n % 10 ==  0
+      %w(ten twenty thirty forty fifty sixty seventy eighty ninety)[n/10]
+    else
+        "#{translate n /10 * 10}-#{translate n % 10}".downcase
+    end.capitalize
+  end
+end
