88cc9baf393a2d27a208400d4ace03777b86ab05
nandhini <nandhini31@gmail.com>
2017-06-18 11:38:34 +0100

    8_array.rb

--- session2/3-challenge/8_array.rb
+++ session2/3-challenge/8_array.rb
@@ -5,4 +5,24 @@
 # got_three? ['a', 'a', 'b']  # => false
 # got_three? ['a', 'a', 'a']  # => true
 # got_three? [1, 2, 1, 1]     # => false
+#
+# def got_three?(my_array)
+#
+#
+#   my_array.each_with_index do |element, index|
+#     if element == my_array[index+1] && my_array[index + 1] == my_array[index + 2]
+#
+#       return true
+#     end
+#     false
+#   end
+# end
 
+def got_three?(my_array)
+  my_array.each_cons 3 do |a,b,c|
+    if a == b && b == c
+        return true
+    end
+    false
+  end
+end
