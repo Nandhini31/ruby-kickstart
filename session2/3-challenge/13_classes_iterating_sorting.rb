1ae6b83a730b7b5c6cd8619279be0a1c69887ffc
nandhini <nandhini31@gmail.com>
2017-06-21 15:15:53 +0100

    13_classes_iterating_sorting.rb

--- session2/3-challenge/13_classes_iterating_sorting.rb
+++ session2/3-challenge/13_classes_iterating_sorting.rb
@@ -1,23 +1,47 @@
-# DO NOT STRUGGLE ON THIS PROBLEM FOR MORE THAN 30 MINUTES!!
 
-# In this exercise you'll need to create two classes.
 
-# One class is called User that keeps track of a person's username
-# it should receive the username when initialized
-# it should have a method add_blog which accepts a date and text
-# it should have a method blogs which returns an array of all blogs the user has written
-# they should be in reverse chronological order (newest first)
-#
-# The other class is called Blog that could be used to store an entry for your web log.
-# The class should have a getter and setter methods: text , date , user
-# its initialize method should receive the date, user , and text
-# have a method called summary that returns the first 10 words from the text (or the entire text if it is less than 10 words)
-#
-# Two blogs should be equal to eachother if they have the same user, date, and text
-# here is a partially filled out example of how to define the == operator:
-#      def ==(other)
-#        return self.date == other.date
-#      end
+require 'date'
+
+class User
+  attr_accessor :username, :blogs
+
+  def initialize(username)
+    self.username = username
+    self.blogs    = []
+  end
+
+  def add_blog(date, text)
+    added_blog = Blog.new(date, self, text)
+    blogs << added_blog
+    self.blogs = blogs.sort_by { |blog| blog.date }.reverse
+    added_blog
+  end
+end
+
+
+class Blog
+  attr_accessor :date, :user, :text
+
+  def initialize(date, user, text)
+    self.date = date
+    self.user = user
+    self.text = text
+  end
+
+  def summary
+    text.split[0..9].join(' ')
+  end
+
+  def entry
+    "#{user.username} #{date}\n#{text}"
+  end
+
+  def ==(other)
+    date   == other.date &&
+      user == other.user &&
+      text == other.text
+  end
+end
 
 
 
@@ -31,7 +55,7 @@
 # lissa.blogs                     # => [#<Blog:0x007fec28c49b88
 #     @date=#<Date: 2010-05-28 ((2455345j,0s,0n),+0s,2299161j)>,
 #     @text="Sailor Mars is my favourite",
-#       @user=#<User:0x007fec2902e5c8 @blogs=[...], @username="QTSort">>] 
+#       @user=#<User:0x007fec2902e5c8 @blogs=[...], @username="QTSort">>]
 #
 # blog1 = lissa.blogs.first
 # blog1.user                      # => lissa
@@ -72,4 +96,3 @@
 
 # date docs are at: http://ruby-doc.org/core/classes/Date.html
 # don't spend too much time worrying about them :)
-require 'date'
