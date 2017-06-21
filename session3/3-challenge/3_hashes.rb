f3f79c3825c7b9dfc0bb828daf29e3bebcdec326
nandhini <nandhini31@gmail.com>
2017-06-21 11:33:19 +0100

    3_hashes.rb

--- session3/3-challenge/3_hashes.rb
+++ session3/3-challenge/3_hashes.rb
@@ -6,3 +6,19 @@
 #
 # Example:
 # word_count "The dog and the cat" # => {"the" => 2, "dog" => 1, "and" => 1, "cat" => 1}
+
+
+
+
+def word_count(string)
+
+  return_hash = Hash.new(0)
+
+  new_array = string.split
+  new_array.each do |i|
+    return_hash[i.downcase] += 1
+
+  end
+  return return_hash
+
+  end
